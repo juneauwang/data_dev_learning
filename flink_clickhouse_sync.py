8e6ff876910e1b0e199976789034ae2dc70ba080
juneauwind <wangweipeng_nob@163.com>
2026-03-05 14:47:50 +0800

    new flink clickhouse v7

--- flink_clickhouse_sync.py
+++ flink_clickhouse_sync.py
@@ -79,12 +79,13 @@ def run_sync_job(**kwargs):
             `current_price` DOUBLE,
             `updated_at` TIMESTAMP_LTZ(6)
         ) WITH (
-            'connector' = 'clickhouse',
+            'connector' = 'jdbc',
             'url' = 'jdbc:clickhouse://clickhouse:8123/default',
             'table-name' = 'crypto_prices_sink',
+            'driver' = 'com.clickhouse.jdbc.ClickHouseDriver',
             'username' = 'default',
             'password' = '',
-            'sink.batch-size' = '500'
+            'sink.buffer-flush.max-rows' = '500'
         )
         """
         execute_sql(session_url, session_handle, create_ch_sink)
